4beda20b37aa43cc5bd3f6ab368bc1b46695967e
elebow <elebow@users.noreply.github.com>
2018-03-29 22:51:28 -0400

    Introduce pipenv

--- startserver.sh
+++ startserver.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-. virtualenv/bin/activate
 
 export FLASK_DEBUG=1
 export FLASK_APP=noteboat.py
@@ -8,4 +7,4 @@ export FLASK_APP=noteboat.py
 export NOTEBOAT_BASE_DIR=$PWD
 
 cd noteboat
-flask run --host=0.0.0.0
+pipenv run flask run --host=0.0.0.0
